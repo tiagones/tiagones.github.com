491a44551d2bfb6c23f064ac5b43510f3df3fb1c
tiagones <tiagones@gmail.com>
2018-07-20 12:08:32 -0300

    Update bower depedency version and post install script

--- bower-postinstall.bat
+++ bower-postinstall.bat
@@ -1,1 +1,1 @@
-RENAME "C:\wamp\www\tiagones.github.com\assets\_src\bower_components\normalize-css\normalize.css" "normalize.scss"
+copy "assets\_src\bower_components\normalize-css\normalize.css" "assets\_src\bower_components\normalize-css\normalize.scss"
